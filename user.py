5912fab2bd6e4fbdc798714a539b612dbe605863
Watcharin <watcharin9581@gmail.com>
2023-03-15 16:15:05 +0700

    <chore>[add class]: Admin, Student, Teacher

--- user.py
+++ user.py
@@ -13,3 +13,18 @@ class User():
         self._province = province 
         self._login = login  # Composition of Login
     
+class Admin(User):
+    def __init__(self, course, course_recom, username, password, email, first_name,last_name, gender, birth_date, education, country, province, login):
+        super().__init__(username, password, email, first_name,last_name, gender, birth_date, education, country, province, login)
+        self.__course = course # Association of Courses 
+        self.__course_recom = course_recom # Uni-di-Association of CourseRecom
+
+class Student(User):
+    def __init__(self, user, enroll, username, password, email, first_name,last_name, gender, birth_date, education, country, province, login):
+        super().__init__(username, password, email, first_name,last_name, gender, birth_date, education, country, province, login)
+        self.__enroll = enroll #  # Uni-di-Association of Enroll 
+
+class Teacher(User):
+    def __init__(self, teacher_dept, user, username, password, email, first_name,last_name, gender, birth_date, education, country, province, login):
+        super().__init__(username, password, email, first_name,last_name, gender, birth_date, education, country, province, login)
+        self.__teacher_dept = teacher_dept 
